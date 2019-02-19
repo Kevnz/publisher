714ee03ce3766ec527f6e3ff7152ba7c3bafe27c
Kevin Isom <kevin.isom@gmail.com>
2019-02-20 01:02:21 +1300

    format

--- src/publish.js
+++ src/publish.js
@@ -2,13 +2,14 @@ const path = require('path')
 const ghpages = require('gh-pages')
 const fse = require('fs-extra')
 
-module.exports = () => new Promise((resolve, reject) => {
-  return fse.ensureDir(path.resolve(process.cwd(), 'dist')).then(()=> {
-    return ghpages.publish(path.resolve(process.cwd(), 'dist'), (err) => {
-      if (err) {
-        return reject(err)
-      }
-      return resolve(true)
+module.exports = () =>
+  new Promise((resolve, reject) => {
+    return fse.ensureDir(path.resolve(process.cwd(), 'dist')).then(() => {
+      return ghpages.publish(path.resolve(process.cwd(), 'dist'), err => {
+        if (err) {
+          return reject(err)
+        }
+        return resolve(true)
+      })
     })
   })
-})
