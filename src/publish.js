0daa8cd59b6a889c6347ddd05a01d6569af46c69
Kevin Isom <kevin.isom@gmail.com>
2019-02-23 16:22:32 +1300

    Add message to gh-pages commit and version bump

--- src/publish.js
+++ src/publish.js
@@ -5,11 +5,17 @@ const fse = require('fs-extra')
 module.exports = () =>
   new Promise((resolve, reject) => {
     return fse.ensureDir(path.resolve(process.cwd(), 'dist')).then(() => {
-      return ghpages.publish(path.resolve(process.cwd(), 'dist'), err => {
-        if (err) {
-          return reject(err)
+      return ghpages.publish(
+        path.resolve(process.cwd(), 'dist'),
+        {
+          message: 'Publisher generated commit [ci skip]',
+        },
+        err => {
+          if (err) {
+            return reject(err)
+          }
+          return resolve(true)
         }
-        return resolve(true)
-      })
+      )
     })
   })
